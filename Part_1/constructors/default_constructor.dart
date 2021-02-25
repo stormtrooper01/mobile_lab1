af747c120f2d686549722043fceee83ee31709da
Yelena <lena.skripnik.01@gmail.com>
2021-02-25 21:02:06 +0200

    Create default_constructor.dart

---
+++ Part_1/constructors/default_constructor.dart
@@ -0,0 +1,13 @@
+//Default constructor
+//Creating Class named Star 
+class Star{ 
+  //Creating Constructor 
+  Star() { 
+    print('May the Force be with you'); 
+  } 
+} 
+  
+void main() { 
+  //Creating Instance of class  
+  Star geek = new Star();   
+}
