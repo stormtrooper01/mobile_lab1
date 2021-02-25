584b22a0372efa11c76ed0ed438f19b691d29b94
Yelena <lena.skripnik.01@gmail.com>
2021-02-25 21:06:25 +0200

    Create parameterized_constructor.dart

---
+++ Part_1/constructors/parameterized_constructor.dart
@@ -0,0 +1,13 @@
+//Parameterized constructor
+//Creating class named Star 
+class Star{ 
+  //Creating Parameterized constructor 
+  Star(int a) { 
+    print('You can go about your business'); 
+  } 
+} 
+  
+void main() { 
+  //Creating Instance of class  
+  Star geek = new Star(1);   
+}
