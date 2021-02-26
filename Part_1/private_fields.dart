9591dbaec9db7e6ba371b51c7dacd74f374c633a
Yelena <lena.skripnik.01@gmail.com>
2021-02-26 10:12:36 +0200

    Create private_fields.dart

---
+++ Part_1/private_fields.dart
@@ -0,0 +1,11 @@
+class Star{ 
+  Star() { 
+    int _private = 16;
+    print('May the Force be with you'); 
+    print('This qoute was said ${_private} times');
+  } 
+} 
+  
+void main() {  
+  Star geek = new Star();   
+}
